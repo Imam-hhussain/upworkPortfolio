0695bde9f4758875f662278578f704e8f87d902a
Your Name <you@example.com>
2025-01-10 16:51:03 +0500

    fixed import issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
 import HeroSection from "../components/heroSection"
 import Services from "../components/services"
 import Testimonial from "../components/testimonials"
-import HireMe from "../components/hireme"
+import HireMe from "../components/hireMe"
 import Experience from "../components/experince"
+
 function Home() {
   return (
     <div>
       <HeroSection/>
        <Services/>
       <Experience/>
-       <HireMe/>
+     <HireMe/>
        <Testimonial/>
     </div>
   )

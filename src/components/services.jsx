cf0bab8718c6ad5bdc36eb8f03e6552c79071ed0
Your Name <you@example.com>
2025-01-10 16:55:51 +0500

    fixed image issue

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,6 +1,4 @@
 import uiux from "../../public/assets/serviceImage.png";
-import web from "../../public/assets/serviceimage.png";
-
 function Services() {
   return (
     <div className="mt-[210px] bg-gray-800 text-white rounded-3xl">
@@ -29,7 +27,7 @@ function Services() {
         <div className=" overflow-hidden border rounded-3xl border-gray-500 mb-12  ">
           <h2 className="text-xl font-bold mb-4 text-center pt-5">App Dev</h2>
           <hr className="border-t-2 border-gray-500 mb-4" />
-          <img src={web} alt="UI/UX Design" className="w-full mt-6" />
+          <img src={uiux} alt="UI/UX Design" className="w-full mt-6" />
         </div>
       </div>
     </div>
